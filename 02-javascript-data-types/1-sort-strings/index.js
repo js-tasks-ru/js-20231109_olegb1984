8ddee1ec1d7183287cda163eb0d541d64d02563a
OBarbashev <obarbashev@sportmasterlab.net>
2023-11-12 17:48:12 +0300

    устранение замечаний по ревью

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -1,10 +1,20 @@
 
 export function sortStrings(arr, param) {
-  let result = arr.slice(0, arr.length+1)
+  const result = [...arr];
 	result.sort((a, b) => a.localeCompare(b, 'ru-RU-u-kf-upper'));
 	(param == 'asc') ? result :
-	(param == 'desc') ? result.reverse() :
+	(param == 'desc') ? reverseArray(result) :
 	 result
 	return result;
+}
+
+function reverseArray(arr){
+let lastElem = arr.length-1;
+
+  for(let i=0; i<lastElem; i++){
+   [arr[i], arr[lastElem]] = [arr[lastElem], arr[i]];
+   
+   lastElem--;
+  }
 }
 
