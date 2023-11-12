fab5ae03a723a5884c02b06547cf7c2c1e44a3bb
OBarbashev <obarbashev@sportmasterlab.net>
2023-11-12 21:35:18 +0300

    Реализована функция pick

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -1,9 +1,8 @@
-/**
- * pick - Creates an object composed of the picked object properties:
- * @param {object} obj - the source object
- * @param {...string} fields - the properties paths to pick
- * @returns {object} - returns the new object
- */
 export const pick = (obj, ...fields) => {
-
+    let pickMap = new Map();
+    for (let key in fields) {
+        pickMap.set(fields[key], obj[fields[key]]);
+    }
+    let newObj = Object.fromEntries(pickMap);
+    return newObj;
 };
