eb1d8e143e3bef7d0c84d04b01e5c842c776bad2
OBarbashev <obarbashev@sportmasterlab.net>
2023-11-14 20:35:36 +0300

    правки по ревью

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -2,7 +2,6 @@ export function sortStrings(arr, param) {
   const result = [...arr];
   const sortingAsc = (a, b) => a.localeCompare(b, ["ru", "en"], {caseFirst: "upper"});
   const sortingDesc = (a, b) => b.localeCompare(a, ["ru", "en"], {caseFirst: "upper"});
-  const sortedArr = result.sort(param === 'asc' ? sortingAsc : param === 'desc' ? sortingDesc : sortingAsc);
-  return sortedArr;
+  return result.sort(param === 'desc' ? sortingDesc : sortingAsc);
 }
 
--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -1,8 +1,7 @@
 export const pick = (obj, ...fields) => {
-    const pickMap = new Map();
-    for (let key in fields) {
-        pickMap.set(fields[key], obj[fields[key]]);
+    const newObj = {};
+    for (const key in fields) {
+      newObj[fields[key]] = obj[fields[key]];
     }
-  const newObj = Object.fromEntries(pickMap);
     return newObj;
 };
