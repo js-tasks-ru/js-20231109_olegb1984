1a634a3ca431187ee2640fb3d0fe48f6e6e3f4f8
OBarbashev <obarbashev@sportmasterlab.net>
2023-11-16 00:30:47 +0300

    реализована функция omit

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,9 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
-
+  let newObj = {...obj};
+  for (const key in fields) {
+    delete newObj[fields[key]];
+  }
+  return newObj;
 };
